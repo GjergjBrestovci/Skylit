9a7156c007a1efc8188077a716a4aa56c3fb22b8
GjergjBrestovci <gjergj.brestovci@gmail.com>
2025-11-03 11:03:57 +0100

    commit

--- apply-migration-auto.js
+++ apply-migration-auto.js
@@ -13,7 +13,7 @@ if (fs.existsSync(envPath)) {
     }
   });
 }
-
+ 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY;
 
